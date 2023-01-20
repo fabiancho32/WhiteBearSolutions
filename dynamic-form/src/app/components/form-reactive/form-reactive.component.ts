2efbb2a9625e3703608782853c258e8270660eda
unknown <fabian.desarrollo@gmail.com>
2023-01-20 13:58:42 -0500

    Add inputs wanted

--- dynamic-form/src/app/components/form-reactive/form-reactive.component.ts
+++ dynamic-form/src/app/components/form-reactive/form-reactive.component.ts
@@ -1,8 +1,10 @@
+import { property } from './../../models/property-model';
 import { properties } from './../../models/properties-model';
 import { FormsService } from './../../services/forms/forms-service.service';
 import { form } from 'src/app/models/form-model';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-reactive',
@@ -13,10 +15,14 @@ export class FormReactiveComponent implements OnInit {
 
   public myForm: FormGroup = this.formBuilder.group({})
   public form: form = {};
-
+  public isResponseButton: boolean = false;
+  public showAlertSuccess: boolean = false;
+  public showAlertError: boolean = false;
+  public showAlertErrorFatal:boolean = false;
 
   constructor(private formBuilder: FormBuilder,
-    private formService: FormsService) { }
+    private formService: FormsService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.getForm();
@@ -24,7 +30,7 @@ export class FormReactiveComponent implements OnInit {
 
 
   private getForm() {
-    this.formService.getForm(3).subscribe({
+    this.formService.getForm(1).subscribe({
       next: data => {
         this.form = data;
         if (this.form.properties) {
@@ -40,7 +46,7 @@ export class FormReactiveComponent implements OnInit {
   createForm(controls: properties) {
     for (const [key, value] of Object.entries(controls)) {
       const validatorsToAdd = [];
-      console.log(key,value);
+      console.log(key, value);
       if (this.verificateRequired(key)) {
         validatorsToAdd.push(Validators.required);
       }
@@ -52,7 +58,7 @@ export class FormReactiveComponent implements OnInit {
       }
       this.myForm.addControl(
         key,
-        this.formBuilder.control((value.value ? value.value : (value.default? value.default : null)), validatorsToAdd)
+        this.formBuilder.control((value.value ? value.value : (value.default ? value.default : null)), validatorsToAdd)
       );
     }
   }
@@ -81,13 +87,20 @@ export class FormReactiveComponent implements OnInit {
     console.log('Form values: ', this.myForm.value);
     console.log('Form values: ', this.myForm.invalid);
 
-    this.formService.sendForm(this.form,1).subscribe({
+    this.formService.sendForm(this.form, 1).subscribe({
       next: data => {
-        alert("Ha sido enviado el formulario correctamente.");
+        this.showAlertSuccess = true;
+        this.isResponseButton = this.validateResponse(data);
+        console.log(this.validateResponse(data));
         this.form = data;
       },
-      error: data => {
-        this.form = data;
+      error: error => {
+        if (error.status == '400') {
+          this.showAlertError = true;
+          this.form = error.error;
+        }else{
+          this.showAlertErrorFatal = true;
+        }
       }
     })
   }
@@ -101,6 +114,23 @@ export class FormReactiveComponent implements OnInit {
       return regexPattern;
     }
   }
-  
+
+  validateResponse(data: form): boolean {
+    if (data.properties) {
+      for (const [key, value] of Object.entries(data.properties)) {
+
+        if (value.type == 'button') {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  onNavigate(button: property) {
+    alert('hola!');
+    this.router.navigateByUrl((button.value).toString());
+  }
+
 
 }
